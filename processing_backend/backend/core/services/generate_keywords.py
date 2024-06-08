d6ee5209a40435773cca3dd8d475078ee3d66d1b
Gheorghe Morari <53918731+GheorgheMorari@users.noreply.github.com>
2024-06-08 18:52:49 +0300

    Update generate_keywords.py

--- processing_backend/backend/core/services/generate_keywords.py
+++ processing_backend/backend/core/services/generate_keywords.py
@@ -2,14 +2,17 @@ import nltk
 
 nltk.download("punkt")
 nltk.download("stopwords")
+nltk.download("wordnet")
 
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
 
+lemmatizer = WordNetLemmatizer()
 stop_words = set(stopwords.words("english"))
 
 
 def generate_keywords(text: str) -> list[str]:
     word_tokens = word_tokenize(text.lower())
-
-    return [word for word in word_tokens if word not in stop_words]
+    filtered_tokens = [word for word in word_tokens if word not in stop_words]
+    return [lemmatizer.lemmatize(word) for word in filtered_tokens]
