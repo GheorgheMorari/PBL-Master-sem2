f77b870b16544554dcc8f02db9f66ee566bb142c
Gheorghe Morari <53918731+GheorgheMorari@users.noreply.github.com>
2024-06-08 18:03:56 +0300

    Rename entrypoint variables for compressor api

--- processing_backend/backend/image_compressor/entrypoints/compressor_api.py
+++ processing_backend/backend/image_compressor/entrypoints/compressor_api.py
@@ -3,13 +3,13 @@ import http
 from fastapi import FastAPI, HTTPException
 
 from processing_backend.backend.core.domain.image_file import B64ImageFile
-from processing_backend.backend.image_compressor.entrypoints import COMPRESSOR_API_ENTRY_POINT_PREDICT_CAPTION_PATH
+from processing_backend.backend.image_compressor.entrypoints import COMPRESSOR_API_ENTRY_POINT_COMPRESS_IMAGE_PATH
 from processing_backend.backend.image_compressor.services.image_compressor import compress_image
 
 app = FastAPI()
 
 
-@app.post(COMPRESSOR_API_ENTRY_POINT_PREDICT_CAPTION_PATH, response_model=B64ImageFile)
+@app.post(COMPRESSOR_API_ENTRY_POINT_COMPRESS_IMAGE_PATH, response_model=B64ImageFile)
 def route_predict_caption(b64_image_file: B64ImageFile) -> B64ImageFile:
     try:
         return compress_image(b64_image_file)
