9c5eefc105f3da97b9e8173d548e67dcf00ee574
Gheorghe Morari <53918731+GheorgheMorari@users.noreply.github.com>
2024-06-09 17:41:45 +0300

    Update metadata

--- processing_backend/backend/core/domain/image_metadata.py
+++ processing_backend/backend/core/domain/image_metadata.py
@@ -4,7 +4,5 @@ from pydantic import BaseModel
 class ImageMetadata(BaseModel):
     user_id: str
     keywords: list[str] = None
-    location: list[str] = None
-    combined_location_keywords: list[str] = None
     compressed: bool = False
     processed: bool = False
