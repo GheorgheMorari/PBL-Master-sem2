87f940b41ce6dd3b928c79a8adb967d20086ac66
Gheorghe Morari <53918731+GheorgheMorari@users.noreply.github.com>
2024-06-11 15:21:55 +0300

    Update image_compressor.py

--- processing_backend/backend/image_compressor/services/image_compressor.py
+++ processing_backend/backend/image_compressor/services/image_compressor.py
@@ -3,6 +3,6 @@ from processing_backend.backend.core.domain.image_file import B64ImageFile
 IMAGE_COMPRESSION_QUALITY = 70
 
 
-def compress_image(b64_image_file: B64ImageFile) -> B64ImageFile:
+def compress_image(b64_image_file: B64ImageFile, quality: int = IMAGE_COMPRESSION_QUALITY) -> B64ImageFile:
     original_image = b64_image_file.to_pil_image()
-    return B64ImageFile.from_pil_image(original_image, quality=IMAGE_COMPRESSION_QUALITY)
+    return B64ImageFile.from_pil_image(original_image, quality=quality)
